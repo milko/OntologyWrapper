40acd7466880ade7292e02d134575023c6f3a353
Milko Škofič <info@skofic.net>
2014-07-23 20:53:36 +0200

    Working on inventory standards: added biological attributes to taxon standard (need to determine which fields are sets and which enums). Threat standards must be revised: need to determine whether country assessment should be a list of structs.

--- Library/test/test_Current.php
+++ Library/test/test_Current.php
@@ -188,164 +188,42 @@ exit;
 */
 
 	//
-	// Many fields.
+	// Get FCU structure offsets flattened.
 	//
-	echo( '<h4>Many fields</h4>' );
+	echo( '<h4>Get FCU structure offsets flattened</h4>' );
 	echo( kSTYLE_TABLE_PRE );
 	echo( kSTYLE_ROW_PRE );
-	echo( kSTYLE_HEAD_PRE );
-	echo( 'Request:' );
-	echo( kSTYLE_HEAD_POS );
-	echo( kSTYLE_ROW_POS );
-	echo( kSTYLE_ROW_PRE );
-	echo( kSTYLE_HEAD_PRE );
-	$param = array
-	(
-	//	kAPI_PAGING_LIMIT => 10,
-		kAPI_PARAM_LOG_REQUEST => TRUE,
-		kAPI_PARAM_CRITERIA => array
-		(
-			':test:feature1' => array
-			(
-				kAPI_PARAM_INPUT_TYPE => kAPI_PARAM_INPUT_STRING
-			),
-			':test:feature2' => array
-			(
-				kAPI_PARAM_INPUT_TYPE => kAPI_PARAM_INPUT_RANGE,
-				kAPI_PARAM_RANGE_MIN => 10,
-				kAPI_PARAM_RANGE_MAX => 20,
-				kAPI_PARAM_OPERATOR => array
-				(
-					kOPERATOR_IRANGE
-				)
-			),
-			':test:feature1/:predicate:SCALE-OF/:test:scale1' => array
-			(
-				kAPI_PARAM_INPUT_TYPE => kAPI_PARAM_INPUT_STRING,
-				kAPI_PARAM_PATTERN => 'two',
-				kAPI_PARAM_OPERATOR => array
-				(
-					kOPERATOR_CONTAINS,
-					kOPERATOR_NOCASE
-				)
-			),
-			':test:feature1/:predicate:SCALE-OF/:test:scale2' => array
-			(
-				kAPI_PARAM_INPUT_TYPE => kAPI_PARAM_INPUT_DEFAULT,
-				kAPI_PARAM_PATTERN => 21
-			),
-			':test:feature2/:predicate:SCALE-OF/:test:scale1' => array
-			(
-				kAPI_PARAM_INPUT_TYPE => kAPI_PARAM_INPUT_STRING,
-				kAPI_PARAM_PATTERN => 'three',
-				kAPI_PARAM_OPERATOR => array
-				(
-					kOPERATOR_CONTAINS,
-					kOPERATOR_NOCASE
-				)
-			),
-			':test:feature2/:predicate:SCALE-OF/:test:scale2' => array
-			(
-				kAPI_PARAM_INPUT_TYPE => kAPI_PARAM_INPUT_STRING,
-				kAPI_PARAM_PATTERN => 'FOUR',
-				kAPI_PARAM_OPERATOR => array
-				(
-					kOPERATOR_CONTAINS,
-					kOPERATOR_NOCASE
-				)
-			),
-			':test:feature2/:predicate:SCALE-OF/:test:scale3' => array
-			(
-				kAPI_PARAM_INPUT_TYPE => kAPI_PARAM_INPUT_ENUM,
-				kAPI_RESULT_ENUM_TERM => array( ':test:enumeration:2' )
-			),
-			':test:feature4' => array
-			(
-				kAPI_PARAM_INPUT_TYPE => kAPI_PARAM_INPUT_STRING,
-				kAPI_PARAM_PATTERN => 'test string five',
-				kAPI_PARAM_OPERATOR => array
-				(
-					kOPERATOR_EQUAL
-				)
-			),
-			':test:feature5' => array
-			(
-				kAPI_PARAM_INPUT_TYPE => kAPI_PARAM_INPUT_STRING,
-				kAPI_PARAM_PATTERN => 'test string six',
-				kAPI_PARAM_OPERATOR => array
-				(
-					kOPERATOR_EQUAL
-				)
-			),
-			':unit:version' => array
-			(
-				kAPI_PARAM_INPUT_TYPE => kAPI_PARAM_INPUT_RANGE,
-				kAPI_PARAM_RANGE_MIN => '19770101',
-				kAPI_PARAM_RANGE_MAX => '20140527',
-				kAPI_PARAM_OPERATOR => array
-				(
-					kOPERATOR_IRANGE
-				)
-			),
-			':type:entity' => array
-			(
-				kAPI_PARAM_INPUT_TYPE => kAPI_PARAM_INPUT_ENUM,
-				kAPI_RESULT_ENUM_TERM => array( ':type:entity:123', ':type:entity:125' )
-			),
-			':unit:domain' => array
-			(
-				kAPI_PARAM_INPUT_TYPE => kAPI_PARAM_INPUT_ENUM
-			)
-		),
-	/*	kAPI_PARAM_SHAPE => array( kTAG_TYPE => 'Point',
-								   kTAG_GEOMETRY => array( array( 10, 20 ), 10000 ) ),*/
-	/*	kAPI_PARAM_SHAPE => array( kTAG_TYPE => 'Circle',
-								   kTAG_GEOMETRY => array( array( 10, 20 ), 10 / 3959 ) ),*/
-	/*	kAPI_PARAM_SHAPE => array( kTAG_TYPE => 'Rect',
-								   kTAG_GEOMETRY => array( array( -10, 30 ),
-																 array( -11, 29 ) ) ),*/
-		kAPI_PARAM_SHAPE
-			=> array( kTAG_TYPE => 'Polygon',
-					  kTAG_GEOMETRY
-						=> array( array( array( 12.8199,42.8422 ),
-										 array( 12.8207,42.8158 ),
-										 array( 12.8699,42.8166 ),
-										 array( 12.8678,42.8398 ),
-										 array( 12.8199,42.8422 ) ),
-								  array( array( 12.8344,42.8347 ),
-										 array( 12.8348,42.8225 ),
-										 array( 12.857,42.8223 ),
-										 array( 12.8566,42.8332 ),
-										 array( 12.8344,42.8347 ) ) ) ),
-		kAPI_PARAM_SHAPE_OFFSET => ':shape',
-		kAPI_PARAM_GROUP => kTAG_DOMAIN
-	);
-	$request = "$base_url?op=".kAPI_OP_MATCH_UNITS;
-	$request .= ('&'.kAPI_REQUEST_LANGUAGE.'=en');
-	$request .= ('&'.kAPI_REQUEST_PARAMETERS.'='.urlencode( json_encode( $param ) ));
-	echo( htmlspecialchars($request) );
-	echo( kSTYLE_HEAD_POS );
-	echo( kSTYLE_ROW_POS );
-	echo( kSTYLE_ROW_PRE );
-	echo( kSTYLE_HEAD_PRE );
-	echo( 'Criteria:' );
-	echo( kSTYLE_HEAD_POS );
-	echo( kSTYLE_ROW_POS );
-	echo( kSTYLE_ROW_PRE );
 	echo( kSTYLE_DATA_PRE );
-	echo( '<pre>' ); print_r( $param[ kAPI_PARAM_CRITERIA ] ); echo( '</pre>' );
+	$offsets = $wrapper->collectStructureOffsets( 'struct:fcu', 0 );
+	echo( '<pre>' ); print_r( $offsets ); echo( '</pre>' );
 	echo( kSTYLE_DATA_POS );
 	echo( kSTYLE_ROW_POS );
+	echo( kSTYLE_TABLE_POS );
+	echo( '<hr>' );
+	
+	//
+	// Get FCU structure offsets structured.
+	//
+	echo( '<h4>Get FCU structure offsets structured</h4>' );
+	echo( kSTYLE_TABLE_PRE );
 	echo( kSTYLE_ROW_PRE );
 	echo( kSTYLE_DATA_PRE );
-	var_dump( json_encode( $param ) );
+	$offsets = $wrapper->collectStructureOffsets( 'struct:fcu', 1 );
+	echo( '<pre>' ); print_r( $offsets ); echo( '</pre>' );
 	echo( kSTYLE_DATA_POS );
 	echo( kSTYLE_ROW_POS );
+	echo( kSTYLE_TABLE_POS );
+	echo( '<hr>' );
+	
+	//
+	// Get FCU structure tags structured.
+	//
+	echo( '<h4>Get FCU structure tags structured</h4>' );
+	echo( kSTYLE_TABLE_PRE );
 	echo( kSTYLE_ROW_PRE );
 	echo( kSTYLE_DATA_PRE );
-	$response = file_get_contents( $request );
-	$result = json_decode( $response, TRUE );
-	echo( '<pre>' ); print_r( $result ); echo( '</pre>' );
+	$offsets = $wrapper->collectStructureOffsets( 'struct:fcu', 2 );
+	echo( '<pre>' ); print_r( $offsets ); echo( '</pre>' );
 	echo( kSTYLE_DATA_POS );
 	echo( kSTYLE_ROW_POS );
 	echo( kSTYLE_TABLE_POS );
